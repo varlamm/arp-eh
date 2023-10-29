71588eabbad4ddbf8e3be2ba4950366b77bb4c4a
System Administrator <root@Shuvendus-MacBook-Pro.local>
2023-10-29 09:03:28 +0530

    Made URL dynamic like APP.URL, SANCTUM_STATEFUL_DOMAINS and SESSION_DOMAIN

--- config/session.php
+++ config/session.php
@@ -153,7 +153,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', null),
+    'domain' => env('SESSION_DOMAIN',  @$_SERVER['HTTP_HOST']),
 
     /*
     |--------------------------------------------------------------------------
